5eca04adaea7efc2c578055485b614e60f20e5be
Billy Tat <tat.billy@gmail.com>
2014-03-28 09:00:53 -0700

    Fix VMC_* env var deprecations and add config.ru

--- env.rb
+++ env.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  host = ENV['VMC_APP_HOST']
-  port = ENV['VMC_APP_PORT']
+  host = ENV['VCAP_APP_HOST']
+  port = ENV['VCAP_APP_PORT']
   "<h1>XXXXX Hello from the Cloud! via: #{host}:#{port}</h1>"
 end
 

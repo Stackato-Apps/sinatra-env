3f22c59e6ccfc0af38ff3b9746d631cc7a57a77d
Billy Tat <billy.tat@hpe.com>
2016-09-20 13:13:53 -0700

    Update for Stackato 4.0

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "http://rubygems.org"
 
-ruby '2.0.0'
+ruby '2.3.1'
 
 gem 'sinatra'
 gem 'thin'
+gem 'eventmachine', '1.0.4'
--- env.rb
+++ env.rb
@@ -2,12 +2,6 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  host = ENV['VCAP_APP_HOST']
-  port = ENV['VCAP_APP_PORT']
-  "<h1>XXXXX Hello from the Cloud! via: #{host}:#{port}</h1>"
-end
-
-get '/env' do
   res = ''
   ENV.each do |k, v|
     res << "#{k}: #{v}<br/>"
